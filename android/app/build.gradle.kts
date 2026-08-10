7b2874a359183df650272864acfbb61b89e04eb3
zhuchenyu2008 <132262479+zhuchenyu2008@users.noreply.github.com>
2026-08-10 15:15:13 +0800

    Bump Android client to v1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.zhuchenyu.aicommute"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildFeatures {
